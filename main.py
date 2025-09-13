39895c6f08fcf58f30c26b06a1053bc462c2f3ed
akhjunaid0047 <akhjunaid.047+github@gmail.com>
2025-09-13 15:13:05 +0530

    added cors

--- main.py
+++ main.py
@@ -2,8 +2,22 @@ from fastapi import FastAPI, HTTPException
 from config import client
 from models import SymptomRequest
 from prompt import SYSTEM_PROMPT
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="AI Symptom Checker", version="1.0")
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:4040"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/check_symptoms")
 async def check_symptoms(request: SymptomRequest):
